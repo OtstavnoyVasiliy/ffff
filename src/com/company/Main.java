617f4db1312f74a078d58fc04b3c4260e2e6e39c
OtstavnoyVasiliy <79037020+OtstavnoyVasiliy@users.noreply.github.com>
2021-02-14 11:17:09 +0300

    calc

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,25 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	char a ;
+	double res;
+        //b - первое введенное значение
+        //a - знак
+        //c - второе введенное значение
+        switch (a){
+            case '+':
+                res=b+c;
+                break;
+            case '-':
+                res=b-c;
+                break;
+            case '*':
+                res=b*c;
+                break;
+            case '/':
+                res=b/c;
+                break;
+                //res - результат на вывод
+    }
     }
 }
