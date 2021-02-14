08aa8fcb4e9fa6fff6b1ecabe67df9d93787e383
Nikita Ridny <nikitarudnai@mail.ru>
2021-02-14 11:35:30 +0300

    aa

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,27 +1,36 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	char a ;
-	double res;
-        //b - первое введенное значение
-        //a - знак
-        //c - второе введенное значение
-        switch (a){
+
+        int res;
+        String s = null;
+        Scanner in = new Scanner(System.in);
+        System.out.println("_______" + "Введите первое значение" + "______");
+        int b = in.nextInt();
+        System.out.println("_______" + "Введите знак" + "______");
+        char a = s.charAt(0);
+        System.out.println("_______" + "Введите второе значение" + "______");
+        int c = in.nextInt();
+        switch (a) {
             case '+':
-                res=b+c;
+                res = b + c;
                 break;
             case '-':
-                res=b-c;
+                res = b - c;
                 break;
             case '*':
-                res=b*c;
+                res = b * c;
                 break;
             case '/':
-                res=b/c;
+                res = b / c;
                 break;
-                //res - результат на вывод
-    }
+            //res - результат на вывод    }
+            System.out.println(res);
+        }
+
     }
 }
